af90501daf3d37f161b9357815fd9bf9fd8a350e
Alexsfyridis <asfyridi@uwo.ca>
2026-09-15 22:49:35 -0400

    Delete java/se2203b/assignments directory

--- java/se2203b/assignments/repo/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package se2203b.assignments.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se2203b.assignments.domain.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
